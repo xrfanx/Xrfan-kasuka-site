5fd7fb76956cdf02d6f4a2eff617b1bd88a6faae
Erfan <erfanroozbahani6@gmail.com>
2026-06-19 03:06:22 +0330

    fix: update asset paths in index.html and remove server configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    base: '/Xrfan-kasuka-site/'
-  }
+  base: '/Xrfan-kasuka-site/'
 })
